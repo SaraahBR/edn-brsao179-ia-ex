b4cce15cde33deb7325d423af33f94a16509223f
SaraHernandes <vihernandesbr@gmail.com>
2025-05-16 21:15:29 -0300

    Atividade de Aula

---
+++ exercicios/pratica-1/calculadora-soma.py
@@ -0,0 +1,16 @@
+'''
+2 - Calculadora de Soma
+Desenvolva um programa que soma dois números.
+Use as variáveis numero1 = 12 e numero2 = 14.
+O programa deve calcular a soma e exibir o resultado.
+'''
+
+# Definindo os números
+numero1 = 12
+numero2 = 14
+
+# Calculando a soma
+soma = numero1 + numero2
+
+# Exibindo o resultado
+print("A soma de", numero1, "e", numero2, "é:", soma)
